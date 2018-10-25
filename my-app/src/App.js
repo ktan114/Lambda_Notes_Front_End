e0bcdf24c5c36a0bf80c20544c1b808dcaa50762
ktan114 <ktaneight@gmail.com>
2018-10-25 17:50:38 -0400

    Moved universal CSS to App.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,49 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import axios from "axios";
 
-import './App.css';
-import { ListView } from './components/ListView/ListView';
-import { NoteView } from './components/NoteView/NoteView';
-import CreateNote from './components/CreateNote/CreateNote';
-import Edit from './components/Edit/Edit';  
-import Delete from './components/Delete/Delete';  
+import "./App.css";
+import ListView from "./components/ListView/ListView";
+import { NoteView } from "./components/NoteView/NoteView";
+import CreateNote from "./components/CreateNote/CreateNote";
+import Edit from "./components/Edit/Edit";
+import Delete from "./components/Delete/Delete";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      note: [],
-    }
+      note: []
+    };
   }
-  
+
   componentDidMount() {
     axios
-    .get(`https://ktan-notes.herokuapp.com/notes`)
-    .then(res => {
-      console.log("GET notes", res.data)
-      this.setState({ note: res.data.notes })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .get(`https://ktan-notes.herokuapp.com/notes`)
+      .then(res => {
+        console.log("GET notes", res.data);
+        this.setState({ note: res.data.notes });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div>
-        <Route exact path = '/' render={(props) => (<ListView {...props} note={this.state.note} /> )} />
-        <Route path = '/noteview/:id' render={(props) => (<NoteView {...props} note={this.state.note} /> )} />
-        <Route path = '/createnote' component= { CreateNote } />
-        <Route path = '/edit/:id' component= { Edit } />
-        <Route path = '/delete/:id' component = { Delete }  />
+        <Route
+          exact
+          path="/"
+          render={props => <ListView {...props} note={this.state.note} />}
+        />
+        <Route
+          path="/noteview/:id"
+          render={props => <NoteView {...props} note={this.state.note} />}
+        />
+        <Route path="/createnote" component={CreateNote} />
+        <Route path="/edit/:id" component={Edit} />
+        <Route path="/delete/:id" component={Delete} />
       </div>
     );
   }
