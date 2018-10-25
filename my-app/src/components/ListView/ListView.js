5173157d8556dfcffc9a395975ac9aa284247616
ktan114 <ktaneight@gmail.com>
2018-10-25 14:43:10 -0400

    Remove sidebar logic and imported from subcomponent

--- my-app/src/components/ListView/ListView.js
+++ my-app/src/components/ListView/ListView.js
@@ -1,37 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './ListView.css';
+import "./ListView.css";
+import Sidebar from "../subcomponents/Sidebar/Sidebar";
 
-export const ListView = (props) => {
-
-    return (
-        <div className="Note">
-            <div className= 'Note-feature'> 
-                <h1> Lambda Notes </h1>
-                <button className ='Note-buttons'>
-                    <Link to= '/'> View Your Notes </Link>
-                </button>
-                <button className ='Note-buttons'>
-                    <Link to= '/createnote'> +Create New Note </Link>
-                </button>
-            </div>
-
-            <div className='Note-content'>
-                <h1> Your Notes: </h1> 
-                <div className= 'Note-notesDisplay'>
-                    {props.note.map((note,index) => {
-                        return <div key={index} className = 'Note-notes'> 
-                            <Link to = { `/noteView/${note._id}`} >
-                            <h3> {note.title} </h3>
-                            <p> {note.body} </p>
-                            </Link>
-                        </div>
-                    })}
-                </div>
-            </div>
+export const ListView = props => {
+  return (
+    <div className="Note">
+      <Sidebar />
+      <div className="Note-content">
+        <h1> Your Notes: </h1>
+        <div className="Note-notesDisplay">
+          {props.note.map((note, index) => {
+            return (
+              <div key={index} className="Note-notes">
+                <Link to={`/noteView/${note._id}`}>
+                  <h3> {note.title} </h3>
+                  <p> {note.body} </p>
+                </Link>
+              </div>
+            );
+          })}
         </div>
-    );
-}
-
-
+      </div>
+    </div>
+  );
+};
--- my-app/src/components/NoteView/NoteView.js
+++ my-app/src/components/NoteView/NoteView.js
@@ -1,39 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './NoteView.css';
+import "./NoteView.css";
+import Sidebar from "../subcomponents/Sidebar/Sidebar";
 
-export const NoteView = (props) => {
+export const NoteView = props => {
+  const id = props.match.params.id;
+  const note = props.note.filter(el => el._id === id)[0];
 
-    const id = props.match.params.id;
-    const note = props.note.filter(el => el._id === id)[0];  
-    
-    return (
-        <div className="Note">
-            <div className='Note-feature'> 
-                <h1> Lambda Notes </h1>
-                <button className ='Note-buttons'>
-                    <Link to= '/'> View Your Notes </Link>
-                </button>
-                <button className ='Note-buttons'>
-                    <Link to= '/createnote'> +Create New Note </Link>
-                </button>
-            </div>
-
-            <div className='Note-holder'>
-                <div className='Note-links'>
-                    <Link to= {`/edit/${id}`}> edit </Link>
-                    <Link to = {`/delete/${id}`}> delete </Link>
-                </div>
-                <div className='Note-body'>
-                    <h1> Note Name </h1>
-                    <div className='Note-text'>
-                        <h3> {note.title} </h3>
-                        <p> {note.body} </p>         
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="Note">
+      <Sidebar />
+      <div className="Note-holder">
+        <div className="Note-links">
+          <Link to={`/edit/${id}`}> edit </Link>
+          <Link to={`/delete/${id}`}> delete </Link>
         </div>
-    );
-}
-
+        <div className="Note-body">
+          <h1> Note Name </h1>
+          <div className="Note-text">
+            <h3> {note.title} </h3>
+            <p> {note.body} </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
