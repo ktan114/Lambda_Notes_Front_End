24811e05d75a4cb8d0dd620391c90a0e6c1e03fd
ktan114 <ktaneight@gmail.com>
2018-10-25 20:55:06 -0400

    Axios call to get the note id

--- my-app/src/components/NoteView/NoteView.js
+++ my-app/src/components/NoteView/NoteView.js
@@ -1,29 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 import "./NoteView.css";
 import Sidebar from "../subcomponents/Sidebar/Sidebar";
 
-export const NoteView = props => {
-  const id = props.match.params.id;
-  const note = props.note.filter(el => el._id === id)[0];
+class NoteView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.match.params.id,
+      note: ""
+    };
+  }
 
-  return (
-    <div className="Note">
-      <Sidebar />
-      <div className="Note-holder">
-        <div className="Note-links">
-          <Link to={`/edit/${id}`}> edit </Link>
-          <Link to={`/delete/${id}`}> delete </Link>
-        </div>
-        <div className="Note-body">
-          <h1> Note Name </h1>
-          <div className="Note-text">
-            <h3> {note.title} </h3>
-            <p> {note.body} </p>
+  componentDidMount() {
+    const { id } = this.state;
+    this.retrieveNote(id);
+  }
+
+  retrieveNote(note_id) {
+    axios
+      .get(`https://ktan-notes.herokuapp.com/notes/${note_id}`)
+      .then(res => {
+        this.setState({ note: res.data.note });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    const { note } = this.state;
+    return (
+      <div className="Note">
+        <Sidebar />
+        <div className="Note-holder">
+          <div className="Note-links">
+            <Link to={`/edit/${this.state.id}`}> edit </Link>
+            <Link to={`/delete/${this.state.id}`}> delete </Link>
+          </div>
+          <div className="Note-body">
+            <h1> Note Name </h1>
+            <div className="Note-text">
+              <h3> {note.title} </h3>
+              <p> {note.body} </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+//   return (
+//     <div className="Note">
+//       <Sidebar />
+//       <div className="Note-holder">
+//         <div className="Note-links">
+//           <Link to={`/edit/${id}`}> edit </Link>
+//           <Link to={`/delete/${id}`}> delete </Link>
+//         </div>
+//         <div className="Note-body">
+//           <h1> Note Name </h1>
+//           <div className="Note-text">
+//             {/* <h3> {note.title} </h3>
+//             <p> {note.body} </p> */}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+export default NoteView;
