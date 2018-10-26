fc87ebc776bbefe62c657cfd3e440397467054a5
ktan114 <ktaneight@gmail.com>
2018-10-25 22:18:45 -0400

    Fixed the edit page and functionality

--- my-app/src/components/Edit/Edit.js
+++ my-app/src/components/Edit/Edit.js
@@ -1,70 +1,96 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+import Sidebar from "../subcomponents/Sidebar/Sidebar";
 
 class Edit extends Component {
-    constructor() {
-        super();
-        this.state = {
-            title: '',
-            body: '',
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.match.params.id,
+      title: "",
+      body: ""
+    };
+  }
 
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value })
-    }
+  componentDidMount() {
+    this.retrieveNote(this.state.id);
+  }
 
-    handleSubmit = (e) => {
-        const updatedObj = { title: this.state.title, body: this.state.body }
-        axios
-        .put(`https://ktan-notes.herokuapp.com/notes/${this.props.match.params.id}`, updatedObj)
-        .then(note => {
-            console.log(note)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+  retrieveNote(note_id) {
+    axios
+      .get(`https://ktan-notes.herokuapp.com/notes/${note_id}`)
+      .then(res => {
+        const { note } = res.data;
+        this.setState({ title: note.title, body: note.body });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    render() {
-        return (
-            <div className="Note">
-            <div className='Note-feature'> 
-                <h1> Lambda Notes </h1>
-                <button className ='Note-buttons'>
-                    <Link to= '/'> View Your Notes </Link>
-                </button>
-                <button className ='Note-buttons'>
-                    <Link to= '/createnote'> +Create New Note </Link>
-                </button>
-            </div>
-            
-            <div className='Note-content'>
-                <h1> Edit Note: </h1>
-                <div className='Note-create'>
-                        <input 
-                            className='Note-title'
-                            type='text'
-                            placeholder='Note Title'
-                            name='title'
-                            value = {this.state.title}
-                            onChange = {this.handleChange}
-                        />
-                        <textarea
-                            className='Note-contented'
-                            type='text'
-                            placeholder='Note Content'
-                            name='body'
-                            value = {this.state.body}
-                            onChange = {this.handleChange}
-                        />
-                    </div>
-                <button onClick = {this.handleSubmit} className= 'Note-button'> Update </button>
-            </div>
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    const updatedObj = { title: this.state.title, body: this.state.body };
+    axios
+      .put(
+        `https://ktan-notes.herokuapp.com/notes/${this.props.match.params.id}`,
+        updatedObj
+      )
+      .then(() => {})
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="Note">
+        <Sidebar />
+        <div className="Note__Page">
+          <h1 className="Note__title"> Edit Note: </h1>
+          <div className="Create">
+            <input
+              className="Create__title"
+              type="text"
+              placeholder="Title"
+              name="title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+            <textarea
+              className="Create-content"
+              type="text"
+              placeholder="Type your note"
+              name="body"
+              value={this.state.body}
+              onChange={this.handleChange}
+            />
+            {/* <input
+            className="Note-title"
+            type="text"
+            placeholder="Note Created By"
+            name="createdBy"
+            value={this.state.createdBy}
+            onChange={this.handleChange}
+          /> */}
+            <Link to={`/noteview/${this.state.id}`}>
+              <button
+                onClick={this.handleSubmit}
+                className="Note__button Note__button--mod"
+              >
+                Update
+              </button>
+            </Link>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default Edit;
