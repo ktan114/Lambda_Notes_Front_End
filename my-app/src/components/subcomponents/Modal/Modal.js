84dfed057daf1522ce2c972f551ca1b9969798ff
ktan114 <ktaneight@gmail.com>
2018-10-30 21:16:38 -0400

    Functionality for delete and modal

--- my-app/src/components/subcomponents/Modal/Modal.js
+++ my-app/src/components/subcomponents/Modal/Modal.js
@@ -1,15 +1,27 @@
 import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 import "./Modal.css";
 
-const Modal = ({ handleClose, show, children }) => {
+const Modal = ({ handleClose, show, id }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  const handleDelete = () => {
+    axios
+      .delete(`https://ktan-notes.herokuapp.com/notes/${id}`)
+      .then(() => {})
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
-        {children}
-        <button onClick={handleClose}>close</button>
+        <h1 className="Text">Are you sure you want to delete this?</h1>
+        <Link to="/">
+          <button onClick={handleDelete}>Delete</button>
+        </Link>
+        <button onClick={handleClose}>No</button>
       </section>
     </div>
   );
