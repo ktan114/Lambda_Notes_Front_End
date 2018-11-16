e49cf6fee55c633a83b7e79b66c0d627eb588e90
ktan114 <ktaneight@gmail.com>
2018-11-16 17:56:43 -0500

    Add basic logic to reroute for login

--- my-app/src/components/ListView.js
+++ my-app/src/components/ListView.js
@@ -1,36 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import Sidebar from "../subcomponents/Sidebar/Sidebar";
+import Sidebar from "../subcomponents/Sidebar";
 
-const ListView = props => {
-  return (
-    <div className="Note">
-      <Sidebar />
-      <div className="Note__Page">
-        <h1 className="Note__title"> Your Notes: </h1>
-        <div className="List__notes">
-          {props.notes.map(note => {
-            return (
-              <Link
-                key={note._id}
-                className="List__note__link"
-                to={{
-                  pathname: `/noteView/${note._id}`,
-                  state: { note: note }
-                }}
-              >
-                <div className="List__note">
-                  <h3 className="List__note__h3"> {note.title} </h3>
-                  <p className="List__note__p"> {note.body} </p>
-                </div>
-              </Link>
-            );
-          })}
+class ListView extends Component {
+  // componentDidMount() {
+  //   this.props.history.push("/login");
+  // }
+  render() {
+    return (
+      <div className="Note">
+        <Sidebar />
+        <div className="Note__Page">
+          <h1 className="Note__title"> Your Notes: </h1>
+          <div className="List__notes">
+            {this.props.notes.map(note => {
+              return (
+                <Link
+                  key={note._id}
+                  className="List__note__link"
+                  to={{
+                    pathname: `/noteView/${note._id}`,
+                    state: { note: note }
+                  }}
+                >
+                  <div className="List__note">
+                    <h3 className="List__note__h3"> {note.title} </h3>
+                    <p className="List__note__p"> {note.body} </p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ListView;
