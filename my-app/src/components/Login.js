127108f649d6aed0c8856a29e30461da20a5e2b9
ktan114 <ktaneight@gmail.com>
2018-11-19 16:01:35 -0500

    Created forms and access the login route

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
 
 class Login extends Component {
   constructor(props) {
@@ -10,10 +13,47 @@ class Login extends Component {
     };
   }
 
+  login = () => {
+    const { username, password } = this.state;
+    const userInfo = { username, password };
+
+    axios
+      .post("http://localhost:5000/users/login", userInfo)
+      .then(response => {
+        console.log(response); // token, user info
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleInput = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <h1>Login Page</h1>
+        <form>
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            value={this.state.username}
+            onChange={this.handleInput}
+          />
+          <input
+            name="password"
+            type="text"
+            placeholder="Enter your password"
+            value={this.state.password}
+            onChange={this.handleInput}
+          />
+        </form>
+        <button onClick={this.login}>Login</button>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
       </div>
     );
   }
