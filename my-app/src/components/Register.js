02c1b398dbdb29b6698bfb1b6737360d41ca46a9
ktan114 <ktaneight@gmail.com>
2018-11-19 15:59:53 -0500

    Created the Register page

---
+++ my-app/src/components/Register.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+
+import axios from "axios";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      success: false
+    };
+  }
+
+  // Axios request to login, do a POST to /login
+  // Receive token, user info
+  register = () => {
+    const { username, password } = this.state;
+    const userInfo = { username, password };
+    console.log("userInfo", userInfo)
+
+    axios
+      .post("http://localhost:5000/users/register", userInfo)
+      .then(response => {
+        console.log(response); // token, user info
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleInput = e => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Registration Page</h1>
+        <form>
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            value={this.state.username}
+            onChange={this.handleInput}
+          />
+          <input
+            name="password"
+            type="text"
+            placeholder="Enter your password"
+            value={this.state.password}
+            onChange={this.handleInput}
+          />
+        </form>
+        <button onClick={this.register}>Register</button>
+      </div>
+    );
+  }
+}
+
+export default Register;
