58fa6dd0b9fb1578a84bd0b15da5e2aad809b102
ktan114 <ktaneight@gmail.com>
2018-11-21 17:47:37 -0500

    Styling for sidebar

--- my-app/src/subcomponents/Sidebar.js
+++ my-app/src/subcomponents/Sidebar.js
@@ -9,23 +9,25 @@ const Sidebar = () => {
   return (
     <div className="Sidebar">
       <h1 className="Sidebar__h1"> Lambda Notes </h1>
-      <button className="Note__button">
-        <Link className="Sidebar__links" to="/">
-          {" "}
-          View Your Notes{" "}
-        </Link>
-      </button>
-      <button className="Note__button">
-        <Link className="Sidebar__links" to="/createnote">
-          {" "}
-          + Create New Note{" "}
-        </Link>
-      </button>
-      <button onClick={logOut} className="Note__button Note__button--red">
-        <Link className="Sidebar__links" to="/login">
-          Logout
-        </Link>
-      </button>
+      <div className="Sidebar__Buttons">
+        <button className="Note__button">
+          <Link className="Sidebar__link" to="/">
+            {" "}
+            View Your Notes{" "}
+          </Link>
+        </button>
+        <button className="Note__button">
+          <Link className="Sidebar__link" to="/createnote">
+            {" "}
+            + Create New Note{" "}
+          </Link>
+        </button>
+        <button onClick={logOut} className="Note__button Note__button--red">
+          <Link className="Sidebar__link" to="/login">
+            Logout
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
